46111b788079d666fcf6533b1d867acef4393282
Salih Yildiz <salihyildiz060@gmail.com>
2024-01-05 04:18:03 +0300

    finished

--- CoinFlip.cpp
+++ CoinFlip.cpp
@@ -1,13 +1,15 @@
 #include "CoinFlip.h"
 #include <cstdlib>
-
+#include "User.h"
 using namespace System;
 using namespace System::Collections::Generic;
-using namespace std;
-
 namespace GoldenLuck {
 
-
+	System::Void CoinFlip::CoinFlip_Load(System::Object^ sender, System::EventArgs^ e) {
+		headsBtn->Enabled = false;
+		tailsBtn->Enabled = false;
+		label1->Text = "Credit: " + User::credit;
+	}
 	int CoinFlip::coinFlip() {
 		if (rand() % 2 == 0) return 0;
 		else return 1;
@@ -16,5 +18,54 @@ namespace GoldenLuck {
 	bool CoinFlip::coinResult() {
 		if (coinChoice == coinFlip()) return true;
 		else return false;
+	}
+	bool CoinFlip::canBet() {
+		if (Convert::ToInt32(comboBox->SelectedItem) <= User::credit) {
+			return true;
+		}
+		else return false;
+	}void CoinFlip::resetGame() {
+		label1->Text = "";
+		resultBox->Controls->Clear();
+		headsBtn->Enabled = false;
+		tailsBtn->Enabled = false;
+		buttonBet->Enabled = true;
+	}
+	void CoinFlip::loadResult() {
+		if (pChoice == 1) resultBox->Load("coins\\heads.png");
+		else resultBox->Load("coins\\tails.png");
+		if (pChoice == coinChoice) label1->Text = "You Win.";
+		else label1->Text = "You Lose.";
+
+	}
+	System::Void CoinFlip::headsBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+		resetGame();
+		coinChoice = 1;
+		pChoice = coinFlip();
+		loadResult();
+		resultBox->Refresh();
+
+	}
+	System::Void CoinFlip::tailsBtn_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		resetGame();
+		coinChoice = 0;
+		pChoice = coinFlip();
+		loadResult();
+		resultBox->Refresh();
+
+	}
+	System::Void CoinFlip::buttonBet_Click(System::Object^ sender, System::EventArgs^ e) {
+		resultBox->Image = nullptr;
+		if (canBet()) {
+			tailsBtn->Enabled = true;
+			headsBtn->Enabled = true;
+			buttonBet->Enabled = false;
+			User::credit -= Convert::ToInt32(comboBox->SelectedItem);
+			label1->Text = "Credit: " + User::credit;
+		}
+		else {
+			label1->Text = "You don't have enough credit.";
+		}
 	}
 }
--- CoinFlip.h
+++ CoinFlip.h
@@ -21,12 +21,22 @@ namespace GoldenLuck {
 		int coinFlip();
 		bool coinResult();
 	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::ComboBox^ comboBox;
+	private: System::Windows::Forms::Button^ buttonBet;
 	public:
 
 	public:
 
 	public:
 		int coinChoice;
+		int pChoice;
+		bool canBet();
+		void loadResult();
+		void resetGame();
+		System::Void CoinFlip_Load(System::Object^ sender, System::EventArgs^ e);
+		System::Void buttonBet_Click(System::Object^ sender, System::EventArgs^ e);
+		System::Void headsBtn_Click(System::Object^ sender, System::EventArgs^ e);
+		System::Void tailsBtn_Click(System::Object^ sender, System::EventArgs^ e);
 		CoinFlip(void)
 		{
 			InitializeComponent();
@@ -72,11 +82,14 @@ namespace GoldenLuck {
 			this->tailsBtn = (gcnew System::Windows::Forms::Button());
 			this->resultBox = (gcnew System::Windows::Forms::PictureBox());
 			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->comboBox = (gcnew System::Windows::Forms::ComboBox());
+			this->buttonBet = (gcnew System::Windows::Forms::Button());
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->resultBox))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// headsBtn
 			// 
+			this->headsBtn->Font = (gcnew System::Drawing::Font(L"Segoe UI Black", 20.25F));
 			this->headsBtn->Location = System::Drawing::Point(182, 464);
 			this->headsBtn->Name = L"headsBtn";
 			this->headsBtn->Size = System::Drawing::Size(166, 98);
@@ -87,12 +100,14 @@ namespace GoldenLuck {
 			// 
 			// tailsBtn
 			// 
+			this->tailsBtn->Font = (gcnew System::Drawing::Font(L"Segoe UI Black", 20.25F));
 			this->tailsBtn->Location = System::Drawing::Point(568, 464);
 			this->tailsBtn->Name = L"tailsBtn";
 			this->tailsBtn->Size = System::Drawing::Size(166, 98);
 			this->tailsBtn->TabIndex = 1;
 			this->tailsBtn->Text = L"TAILS";
 			this->tailsBtn->UseVisualStyleBackColor = true;
+			this->tailsBtn->Click += gcnew System::EventHandler(this, &CoinFlip::tailsBtn_Click);
 			// 
 			// resultBox
 			// 
@@ -105,13 +120,40 @@ namespace GoldenLuck {
 			// 
 			// label1
 			// 
-			this->label1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 50));
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI Black", 20.5F));
 			this->label1->Location = System::Drawing::Point(113, 21);
 			this->label1->MaximumSize = System::Drawing::Size(1000, 1000);
 			this->label1->Name = L"label1";
 			this->label1->Size = System::Drawing::Size(720, 130);
 			this->label1->TabIndex = 3;
-			this->label1->Click += gcnew System::EventHandler(this, &CoinFlip::label1_Click);
+			this->label1->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			// 
+			// comboBox
+			// 
+			this->comboBox->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(180)), static_cast<System::Int32>(static_cast<System::Byte>(188)),
+				static_cast<System::Int32>(static_cast<System::Byte>(224)));
+			this->comboBox->FlatStyle = System::Windows::Forms::FlatStyle::Popup;
+			this->comboBox->FormattingEnabled = true;
+			this->comboBox->Items->AddRange(gcnew cli::array< System::Object^  >(7) {
+				L"100", L"500", L"1000", L"2000", L"3000", L"4000",
+					L"5000"
+			});
+			this->comboBox->Location = System::Drawing::Point(394, 464);
+			this->comboBox->Name = L"comboBox";
+			this->comboBox->Size = System::Drawing::Size(121, 21);
+			this->comboBox->TabIndex = 1;
+			this->comboBox->Text = L"0";
+			// 
+			// buttonBet
+			// 
+			this->buttonBet->Font = (gcnew System::Drawing::Font(L"Segoe UI Black", 20.25F));
+			this->buttonBet->Location = System::Drawing::Point(394, 503);
+			this->buttonBet->Name = L"buttonBet";
+			this->buttonBet->Size = System::Drawing::Size(121, 59);
+			this->buttonBet->TabIndex = 5;
+			this->buttonBet->Text = L"BET";
+			this->buttonBet->UseVisualStyleBackColor = true;
+			this->buttonBet->Click += gcnew System::EventHandler(this, &CoinFlip::buttonBet_Click);
 			// 
 			// CoinFlip
 			// 
@@ -120,41 +162,25 @@ namespace GoldenLuck {
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(180)), static_cast<System::Int32>(static_cast<System::Byte>(216)),
 				static_cast<System::Int32>(static_cast<System::Byte>(224)));
 			this->ClientSize = System::Drawing::Size(944, 681);
+			this->Controls->Add(this->buttonBet);
+			this->Controls->Add(this->comboBox);
 			this->Controls->Add(this->label1);
 			this->Controls->Add(this->resultBox);
 			this->Controls->Add(this->tailsBtn);
 			this->Controls->Add(this->headsBtn);
 			this->Name = L"CoinFlip";
 			this->Text = L"CoinFlip";
+			this->Load += gcnew System::EventHandler(this, &CoinFlip::CoinFlip_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->resultBox))->EndInit();
 			this->ResumeLayout(false);
 
 		}
-		void resetGame() {
-			resultBox->Refresh();
-		}
-		void loadResult() {
-			if (coinResult() == true) resultBox->Load("coin\\coins-heads.gif");
-			else resultBox->Load("coin\\coins-tails.gif");
 
-		}
+
 
 #pragma endregion
-	private: System::Void headsBtn_Click(System::Object^ sender, System::EventArgs^ e) {
-		coinChoice = 0;
-		if (coinResult()) label1->Text = "YOU WIN!";
-		else label1->Text = "YOU LOSE!";
-		loadResult();
-	}
-	private: System::Void tailsBtn_Click(System::Object^ sender, System::EventArgs^ e) {
-		coinChoice = 1;
-		if (coinResult()) label1->Text = "YOU WIN!";
-		else label1->Text = "YOU LOSE!";
-		loadResult();
-	}
-	private: System::Void pictureBox1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
-	private: System::Void label1_Click(System::Object^ sender, System::EventArgs^ e) {
-	}
+
+
+
 	};
 }
